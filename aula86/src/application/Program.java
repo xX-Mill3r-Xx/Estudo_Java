cccb8445b3b5c29f230415353fbcd1a79ed3bc9b
MillerVasco <millermartinsvasco1@gmail.com>
2023-02-24 13:51:20 -0300

    add aula86 and aula88

---
+++ aula86/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		int p;
+		//System.out.println(p); // erro: variável não iniciada
+		p = 10;
+		System.out.println(p);
+
+	}
+
+}
