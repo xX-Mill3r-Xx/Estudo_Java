e42e616666723501a448ff1c798ec1b114c7866d
MillerVasco <millermartinsvasco1@gmail.com>
2023-02-25 11:04:02 -0300

    25022023

---
+++ aula91_Correcao/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Quantos numeros voce vai digitar? ");
+		int n = sc.nextInt();
+		
+		int[] vet = new int[n];
+		
+		for(int i = 0; i<n; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = sc.nextInt();
+		}
+		System.out.println("NUMEROS NEGATIVOS:");
+		for(int i = 0; i<n; i++) {
+			if(vet[i]<0) {
+				System.out.println(vet[i]);
+			}
+		}
+		sc.close();
+
+	}
+
+}
